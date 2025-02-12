f53957c4b676a79ed9d904fd86ecd666c0e68ad8
Lse-Yeong <ocdee39@gmail.com>
2025-02-12 12:00:17 +0900

    이진 탐색 함수로 구현 부분 추가

--- python/이진탐색/정렬된 배열에서 특정 수 개수.py
+++ python/이진탐색/정렬된 배열에서 특정 수 개수.py
@@ -3,12 +3,37 @@ import bisect
 N, K = map(int,input().split())
 array = list(map(int,input().split()))
 
-left = bisect.bisect_left(array,K)
-right = bisect.bisect_right(array,K)
 
-diff = right-left
+## bisect 방법
+# left = bisect.bisect_left(array,K)
+# right = bisect.bisect_right(array,K)
 
-if(diff==0):
-    print(-1)
-else:
-    print(diff)
+# diff = right-left
+
+# if(diff==0):
+#     print(-1)
+# else:
+#     print(diff)
+
+def first(arr,k,left,right):
+    if(left>right):
+        return None
+    mid = (left+right)//2
+    if((mid==0 or k>arr[mid-1]) and arr[mid]==k):
+        return mid
+    elif(arr[mid]>=k):
+        return first(arr,k,left,mid-1)
+    else:
+        return first(arr,k,mid+1,right)
+
+def last(arr,k,left,right):
+    if(left>right):
+        return None
+    mid = (left+right)//2
+    if((mid==N-1 or k<arr[mid+1]) and arr[mid]==k):
+        return mid
+    elif(arr[mid]>k):
+        return last(arr,k,left,mid-1)
+    else:
+        return last(arr,k,mid+1,right)
+
